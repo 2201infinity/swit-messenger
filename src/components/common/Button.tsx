24df9b5dd9d7c6f0dcce2b31513303f1df7f1aed
perfumelim <perfumelim@gmail.com>
2022-02-10 21:30:21 +0900

    feat: button/ Input 컴포넌트 구현

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+interface ButtonStyledProps {
+  variant: "primary" | "secondary";
+  width: string;
+  height?: string;
+  borderRadius?: string;
+}
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    ButtonStyledProps {
+  children: React.ReactNode;
+}
+
+export const Button = ({
+  variant,
+  width,
+  height = "36px",
+  children,
+  borderRadius = "4px",
+  ...rest
+}: ButtonProps): JSX.Element => {
+  return (
+    <StyledButton
+      variant={variant}
+      width={width}
+      height={height}
+      borderRadius={borderRadius}
+      {...rest}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button<ButtonStyledProps>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  border-radius: ${({ borderRadius }) => borderRadius};
+  font-size: ${({ theme }) => theme.fontSize.text};
+  box-sizing: border-box;
+  transition: 0.15s ease-in-out;
+
+  ${({ variant }) => {
+    switch (variant) {
+      case "primary":
+        return css`
+          background-color: ${({ theme }) => theme.colors.main};
+          color: ${({ theme }) => theme.colors.button};
+          border: none;
+          &:hover {
+            background-color: ${({ theme }) => theme.colors.buttonHover};
+          }
+        `;
+      case "secondary":
+        return css`
+          background-color: ${({ theme }) => theme.colors.button};
+          color: ${({ theme }) => theme.colors.main};
+          border: 1px solid ${({ theme }) => theme.colors.main};
+          &:hover {
+            background-color: ${({ theme }) => theme.colors.buttonHover};
+            color: ${({ theme }) => theme.colors.button};
+          }
+        `;
+    }
+  }};
+`;
