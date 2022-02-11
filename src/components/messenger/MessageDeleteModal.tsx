f2df99411d6d00f5caca943d9e875b5a7ad417b5
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-11 13:43:50 +0900

    feat: 삭제 모달 말 줄임표 생성

--- src/components/messenger/MessageDeleteModal.tsx
+++ src/components/messenger/MessageDeleteModal.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react";
+import styled from "styled-components";
 import ModalTemplate from "../common/ModalTemplate";
 
 interface MessageDeleteModalProps {
@@ -14,17 +15,32 @@ function MessageDeleteModal({
   content,
   onClick,
 }: MessageDeleteModalProps): ReactElement {
+  const isElipsis = content.length > 10;
+
   return (
     <ModalTemplate
       width={328}
-      height={178}
+      height={90}
       isModal={isModal}
       onToggleModal={onToggleModal}
     >
-      {content}
-      <button onClick={onClick}>확인</button>
+      <DeleteModalInner>
+        {content.substring(0, 10)}
+        {isElipsis && "..."} 메세지를 삭제하시겠습니까?
+        <div>
+          <button onClick={onClick}>삭제</button>
+          <button onClick={onToggleModal}>취소</button>
+        </div>
+      </DeleteModalInner>
     </ModalTemplate>
   );
 }
+
+const DeleteModalInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 20px;
+`;
 
 export default MessageDeleteModal;
