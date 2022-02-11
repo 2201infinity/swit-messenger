faa3a870e0c6cfdf72221adf6a8479e8350a3022
perfumelim <perfumelim@gmail.com>
2022-02-11 23:03:50 +0900

    feat: 뒤로 가기 기능 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { ReactComponent as DeleteIcon } from "./delete_black_18dp.svg";
 export { ReactComponent as ReplyIcon } from "./reply_black_18dp.svg";
+export { ReactComponent as BackIcon } from "./chevron_left_white_24dp.svg";
--- src/components/messenger/ChatHeader.tsx
+++ src/components/messenger/ChatHeader.tsx
@@ -1,11 +1,22 @@
 import { Teamlogo } from "assets/images";
 import styled from "styled-components";
+import { BackIcon } from "assets/icons";
+import { useNavigate } from "react-router-dom";
 
 function ChatHeader() {
+  const navigate = useNavigate();
+
+  const onBacktoLogin = () => {
+    navigate("/");
+  };
+
   return (
     <HeaderContainer>
-      <img src={Teamlogo} alt="team-profile" />
-      <p>8팀 인피니티 회의실</p>
+      <StyledIcon onClick={onBacktoLogin} />
+      <ChatRoomName>
+        <img src={Teamlogo} alt="team-profile" />
+        <p>인피니티 회의실</p>
+      </ChatRoomName>
     </HeaderContainer>
   );
 }
@@ -14,9 +25,9 @@ export default ChatHeader;
 
 const HeaderContainer = styled.div`
   display: flex;
-  justify-content: flex-start;
   align-items: center;
-  padding: 0 40px;
+  margin-bottom: 7px;
+  padding-left: 13px;
   width: 420px;
   height: 64px;
   min-height: 64px;
@@ -28,15 +39,26 @@ const HeaderContainer = styled.div`
   box-shadow: 1px 3px 5px 0px rgba(255, 80, 95, 0.7);
   -webkit-box-shadow: 1px 3px 5px 0px rgba(255, 80, 95, 0.7);
   -moz-box-shadow: 1px 3px 5px 0px rgba(255, 80, 95, 0.7);
-
   text-align: center;
-  font-size: ${({ theme }) => theme.fontSize.text};
+`;
+
+const ChatRoomName = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: ${({ theme }) => theme.fontSize.subTitle};
   color: ${({ theme }) => theme.colors.white};
 
   img {
     border-radius: 50%;
     width: 30px;
     height: 30px;
-    margin-right: 20px;
+    margin-right: 15px;
   }
+`;
+
+const StyledIcon = styled(BackIcon)`
+  margin-right: 77px;
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
 `;
