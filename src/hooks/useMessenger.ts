5279d4cc90b98db54b5235844c4bfcc000946d38
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-11 10:53:40 +0900

    feat: useMessenger hooks 생성

---
+++ src/hooks/useMessenger.ts
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { IMessage } from "types/message";
+import MockMessages from "utils/data.json";
+
+export default function useMessenger() {
+  const [messages, setMessages] = useState<IMessage[]>(MockMessages.messages);
+  const [message, setMessage] = useState<string>("");
+
+  const onSendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (message.trim().length > 0) {
+      setMessages([
+        ...messages,
+        {
+          userId: Date.now(),
+          userName: "도현",
+          profileImage: "https://i.ibb.co/LNw3QCV/image.png",
+          content: message,
+          date: new Date().toLocaleString(),
+        },
+      ]);
+      setMessage("");
+    }
+  };
+
+  const onChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  return { messages, message, onChangeMessage, onSendMessage };
+}
