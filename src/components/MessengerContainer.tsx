60207639d13e386d42999f901ce7aaaf06207e6e
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-11 10:19:40 +0900

    feat: Message 컴포넌트 생성

---
+++ src/components/MessengerContainer.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+function MessengerContainer(): ReactElement {
+  return (
+    <div>
+      <h1>MessengerContainer</h1>
+    </div>
+  );
+}
+
+export default MessengerContainer;
