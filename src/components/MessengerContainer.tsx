0b608a02ea2e1dcb334e246b499a1c7fd48d2a82
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-11 12:50:54 +0900

    feat: Modal Template 생성

--- src/components/MessengerContainer.tsx
+++ src/components/MessengerContainer.tsx
@@ -5,6 +5,7 @@ function MessengerContainer(): ReactElement {
   const { message, messages, onSendMessage, onChangeMessage } = useMessenger();
   const userId = 999888; // @Todo 로그인 구현 되면 redux에 userId 값 가져와서 넣어야함
 
+  // @Note 아래 코드는 모두 임시 코드입니다. 합칠 때 수정해야 합니다.
   return (
     <div>
       <h1>MessengerContainer</h1>
@@ -13,7 +14,7 @@ function MessengerContainer(): ReactElement {
         <div key={index} style={{ textAlign: "left" }}>
           <span style={{ marginRight: "10px" }}>
             {msg.userName}
-            {msg.userId === 999888 && <span>*</span>}
+            {msg.userId === userId && <span>*</span>}
           </span>
           <span>{msg.content}</span>
           <span>{msg.date}</span>
