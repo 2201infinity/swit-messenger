de1fe5953d52ac5ed2a3de2e4b0f8cb0f2df05b7
kykim00 <jeongsaeki@likelion.org>
2022-02-11 12:36:21 +0900

    refactor: ImageBox 컴포넌트 src 데이터 props로 입력 받도록 수정 fix: status 데이터 오류 수정 style: othersChatBackground 컬러 수정 feat: Message 컴포넌트 구현 feat: ChatRoom 컴포넌트 구현

---
+++ src/components/Message.tsx
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components';
+
+interface IMyMessageProps {
+  myMessage?: boolean;
+  children: React.ReactNode;
+}
+
+export const Message = ({ myMessage = false, children }: IMyMessageProps) => {
+  return (
+    <MessageContainer isMyMessage={myMessage}>{children}</MessageContainer>
+  );
+};
+
+const MessageContainer = styled.div<{ isMyMessage: boolean }>`
+  ${(props) =>
+    props.isMyMessage
+      ? css`
+          background: ${({ theme }) => theme.colors.myChatBackground};
+          color: ${({ theme }) => theme.colors.white};
+          margin-right: 10px;
+        `
+      : css`
+          background: ${({ theme }) => theme.colors.othersChatBackground};
+          color: ${({ theme }) => theme.colors.black};
+          margin-left: 10px;
+        `}
+  font-size: ${({ theme }) => theme.fontSize.text};
+  padding: 17px 20px;
+  max-width: 75%;
+  border-radius: 6px;
+  line-height: 1.5;
+`;
