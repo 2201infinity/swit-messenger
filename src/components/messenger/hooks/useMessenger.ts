6564b4e9ee9ea347a893c6c155c06cddb25635f1
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-11 14:27:13 +0900

    feat: 주석 작성

--- src/components/messenger/hooks/useMessenger.ts
+++ src/components/messenger/hooks/useMessenger.ts
@@ -7,6 +7,7 @@ export default function useMessenger() {
   const [messages, setMessages] = useState<IMessage[]>(MockMessages.messages);
   const [message, setMessage] = useState<string>("");
 
+  // @Note 임시로 만든 메세지 보내는 함수 입니다. 나중에 예지님이 구현하신 코드로 대체할 예정입니다~
   const onSendMessage = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (message.trim().length > 0) {
